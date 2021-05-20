052b3b4df8aa58799fbb72497cfa3530c25a89fa
Chizhikov <57985484+SergioChizhikov@users.noreply.github.com>
2021-05-20 10:36:21 +0300

    На 10,35 20.05

---
+++ Aroma/php/db.php
@@ -0,0 +1,3 @@
+<?php
+$mysqli = mysqli_connect("localhost", "napli_sdo_0407", "123456", "napli_sdo_0407");
+?>
--- Aroma/router.php
+++ Aroma/router.php
@@ -1,17 +1,21 @@
 <?php
 $url = explode("/", $_SERVER['REQUEST_URI']);
+require_once("php/classes/User.php");
+require_once("php/db.php");
 
-if($url[1] == auth){
+if ($url[1] == auth) {
     $content = file_get_contents("pages/login.php");
-}else if($url[1] == reg){
+} else if ($url[1] == reg) {
     $content = file_get_contents("pages/register.html");
-}else if($url[1] == users) {
-require_once("pages/users/index.html");
-}else{
+} else if ($url[1] == users) {
+    require_once("pages/users/index.html");
+} else if ($url[1] == addUser) {
+    User::addUser();
+} else {
     $content = file_get_contents("pages/index.php");
 }
 
-if(!empty($content)) require_once("template.php");
+if (!empty($content)) require_once("template.php");
 
 // if($url[1] == blog) {
 //     require_once("blog.php");
